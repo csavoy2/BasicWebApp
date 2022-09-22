d3a27bdb803c123d0c7f2c83e608b5499f06315b
csavoy2 <83295311+csavoy2@users.noreply.github.com>
2022-09-22 08:37:22 -0400

    adding secret

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome back!</h1>" +
                 "<p>Enter your new query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
